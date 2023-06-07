3dbac446bf334836c61bee8f579bc0c10d746323
Alejandro Diaz <alejandrodsfd@gmail.com>
2023-06-07 12:17:58 -0500

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+/*============== SH=OW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')  
+/*============== MENU SHOW ===============*/
+// Validacion si la constante existe
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+/*============== MENU HIDDEN ===============*/
+// Validacion si la constante existe
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*='+ sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+sr.reveal(`.patrocinadores_container, .edicion__2021, .form__container`, {distance: '30px',delay: 5})
+sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
+sr.reveal(`.premios__card, .program__card, .pricing__card`, {distance: '80px',delay: 20, interval: 600})
+sr.reveal(`.detalles__text, .objetivos__container-imagen`, {delay: 5, origin: 'left'})
+sr.reveal(`.detalles__img, .objetivos__container-contenido`, {delay: 5, origin: 'right'})
